a2640683d8887142da3566f8b7b4040af66ea1c3
alemba-21 <alembakiragu@gmail.com>
2022-07-12 15:05:33 +0300

    commiting my calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 // *** Write your code here***
+let display = document.querySelector("#display")
+let clearNum = document.querySelector("#clearNumbers")
+
+
+function calcNumbers(x){
+    display.value = display.value + x;
+}
+
+clearNum.addEventListener('click', ()=>{
+    if ("c"){
+        display.value =""
+    }
+})
+
+function deleteNum(){
+    if("del"){
+        let arr=display.value.split("")
+        arr.pop()
+        display.value=arr.join("")
+    }
+}
